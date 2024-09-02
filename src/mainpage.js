b00d6c49037c917e2c22a0050f95146d9e466f32
ADDikt88 <kyletsang8@gmail.com>
2024-09-02 00:38:48 -0400

    Clean add task function and clear dialog
    Clear dialog when adding a new task.
    Cleaned code to smooth logic for when adding a new task.

--- src/mainpage.js
+++ src/mainpage.js
@@ -64,51 +64,10 @@ function addToDo(currenTaskList, currentProject, container) {
         console.log("UPDATED");
         const position = defaultProject.toDoItems.length - 1;
         updateToDoItems (defaultProject, container, position);
-        //e.preventDefault();
         //libraryContainer.appendChild(myLibrary[myLibrary.length - 1].createBook());
         return true;
     }
 };
-
-function addAction(container, addToDoItemBtn, currentProject) {
-    const dialog = document.querySelector("#addItemDialog");
-    const confirmBtn = document.querySelector("#confirmBtn");
-    const closeDialogBtn = document.querySelector("#closeDialogBtn");
-
-    addToDoItemBtn.addEventListener('click', () => {
-        dialog.showModal();
-    });
-
-    // When the user clicks the confirm button, close the dialog
-    confirmBtn.addEventListener('click', (e) => {
-        //add an item function
-        console.log("CONFIRM");
-        if(addToDo(listOfItems, currentProject, container))
-        {
-            
-            dialog.close();
-            console.log("Close dialogue");
-        }
-        e.preventDefault();        
-        
-
-    });
-
-    // When the user clicks the close button, close the dialog
-    closeDialogBtn.addEventListener('click', (e) => {
-        dialog.close();
-    });
-
-    
-}
-
-    
-
-
-
-
-
-
 
 /****
  * These functions display the project header and the to-do-list frame
@@ -123,57 +82,139 @@ function displayProjectHeader (content, currentProject) {
     projectDescription.textContent = currentProject.description;
     projectHeader.appendChild(projectTitle);
     projectHeader.appendChild(projectDescription);
+
+
+
+    
 }
 
 function displayToDoItems (content, currentProject) {
     const toDoContainer = document.createElement("div");
     toDoContainer.setAttribute("class", "to-do-container");
+    toDoContainer.style.display = "flex";
+    toDoContainer.style.flexDirection = "column";
+    toDoContainer.style.gap = "10px";
     content.appendChild(toDoContainer);
+    
 
     const addToDoItemBtn = document.createElement("button");
     toDoContainer.appendChild(addToDoItemBtn);
     addToDoItemBtn.textContent = "Add Task";
 
+    const dialogForm = document.querySelector("#addActionForm");
+    const dialog = document.querySelector("#addItemDialog");
+
+    addToDoItemBtn.addEventListener('click', (e) => {
+        dialogForm.reset();
+        dialog.showModal();
+        e.preventDefault();
+    });
+
+
+    // When the user clicks the confirm button, close the dialog
+    confirmBtn.addEventListener('click', (e) => {
+        //add an item function
+        console.log("CONFIRM");
+        if(addToDo(listOfItems, currentProject, toDoContainer))
+            dialog.close();
+        e.preventDefault();              
+
+    });
+
+    // When the user clicks the close button, close the dialog
+    closeDialogBtn.addEventListener('click', (e) => {
+        dialog.close();
+        e.preventDefault();    
+    });
+
     updateToDoItems (currentProject, toDoContainer, 0);
-    addAction(toDoContainer, addToDoItemBtn, currentProject);
+
+
 
 
 }
 
 function updateToDoItems (currentProject, container, position) {
 
+    //Create Task Item
     const toDoItem = document.createElement("div");
-    toDoItem.setAttribute("id", "taskID_" + position)
+    toDoItem.setAttribute("id", "taskID_" + position);
+    toDoItem.style.padding = "5px";
+    toDoItem.style.margin = "5px";
     container.appendChild(toDoItem);
 
-    const statusDiv = document.createElement("div");
-    statusDiv.textContent = "Complete?";
-    statusDiv.style.display = "flex";
+    //Create FUNCTIONAL elements
+    //create status element
+    const titleDiv = document.createElement("div");
+    //statusDiv.textContent = "Complete?";
+    titleDiv.style.display = "flex";
+    titleDiv.style.alignItems = "center";
+    titleDiv.style.justifyContent = "flex-start";
+    titleDiv.style.gap = "5px";
+    
     const status = document.createElement("input");
+    status.style.width = "auto";
     status.setAttribute("type", "checkbox");
-    statusDiv.appendChild(status);
-
-    const toDoTitle = document.createElement("div");
-    const toDoDescription = document.createElement("div");
-    const priorityLevel = document.createElement("div");
-
-    toDoTitle.textContent = currentProject.toDoItems[position].title;
-    toDoDescription.textContent = currentProject.toDoItems[position].description;
-    priorityLevel.textContent = currentProject.toDoItems[position].priority;
-
-    toDoItem.appendChild(statusDiv);
-    toDoItem.appendChild(toDoTitle);
-    toDoItem.appendChild(toDoDescription);
-    toDoItem.appendChild(priorityLevel);
+    status.style.transform = "scale(1.5)";
+    status.style.margin = "5px";
+    titleDiv.appendChild(status);
 
     //Checkbox listener for status
-    statusDiv.addEventListener("change", function(e) {
+    status.addEventListener("change", function(e) {
         if (e.target.checked) {
             toDoItem.style.backgroundColor = "#a9f7c7";
         } else {
             toDoItem.style.backgroundColor = "";
         }
     });
+
+    //create edit/delete element
+    const editDelDiv = document.createElement("div");
+    editDelDiv.style.display = "flex";
+    editDelDiv.style.justifyContent = "space-around";
+    
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editDelDiv.appendChild(editBtn);
+
+    //addAction(container, editBtn, currentProject);
+
+
+
+
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "Delete";
+    editDelDiv.appendChild(delBtn);
+
+    //Checkbox listener for status
+    // statusDiv.addEventListener("change", function(e) {
+    //     if (e.target.checked) {
+    //         toDoItem.style.backgroundColor = "#a9f7c7";
+    //     } else {
+    //         toDoItem.style.backgroundColor = "";
+    //     }
+    // });
+
+
+
+
+    //Create STATIC elements
+    const toDoTitle = document.createElement("div");
+    const toDoDescription = document.createElement("div");
+    const priorityLevel = document.createElement("div");
+
+    toDoTitle.textContent = currentProject.toDoItems[position].title;
+    toDoTitle.style.fontSize = "1.5rem";
+    toDoDescription.textContent = currentProject.toDoItems[position].description;
+    priorityLevel.textContent = currentProject.toDoItems[position].priority;
+
+    toDoItem.appendChild(titleDiv);
+    titleDiv.appendChild(toDoTitle);
+    toDoItem.appendChild(toDoDescription);
+    toDoItem.appendChild(priorityLevel);
+    toDoItem.appendChild(editDelDiv);
+
+
 
     console.log(currentProject.toDoItems);
 
