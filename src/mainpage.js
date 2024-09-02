8f9b7841037116436d704f25ef78d7a6eecf8a4b
ADDikt88 <kyletsang8@gmail.com>
2024-09-02 04:28:06 -0400

    Must keep track of which buttons go with each task
    Can store button objects or any functional object
    as part of the task object. If you don't keep track
    you will throw errors. Need to use the event.target.
    
    Program can now add, edit, delete without problems,
    while tracking which task those buttons are associated
    with.
    
    Need to refactor code once we add more elements.

--- src/mainpage.js
+++ src/mainpage.js
@@ -22,14 +22,8 @@ function createProject (title, description, toDoItems) {
 
 }
 
-function createToDo (title, description, priority, status) {
+function createToDo (title, description, priority, editBtn, delBtn, checkmark, status) {
 
-    function changeStatus (){
-        if (status)
-            this.status = false;
-        else
-            this.status = true;
-    }
 
     function editItem (newTitle, newDesc, newPriority) {
         this.title = newTitle;
@@ -38,13 +32,13 @@ function createToDo (title, description, priority, status) {
     }
 
     return {
-        title, description, priority, status, changeStatus, editItem,
+        title, description, priority, status, editItem, editBtn, delBtn, checkmark,
     }
 
 }
 
 let listOfItems = [];
-listOfItems.push(createToDo("First to do item", "Create an HTML file", "high", false));
+//listOfItems.push(createToDo("First to do item", "Create an HTML file", "high", false));
 
 let defaultProject = createProject ("Default", "This is a default project description", listOfItems);
 
@@ -68,7 +62,8 @@ function addToDo(currenTaskList, currentProject, container) {
         if (taskDescription < 1)
             taskDescription = "_";
 
-        currentProject.toDoItems.push(createToDo(taskTitle, taskDescription, priorityLevel, false));
+        currentProject.toDoItems.push(createToDo(taskTitle, taskDescription, priorityLevel, 
+            undefined, undefined, undefined, false));
         console.log("UPDATED");
         const position = defaultProject.toDoItems.length - 1;
         updateToDoItems (defaultProject, container, position);
@@ -160,7 +155,7 @@ function displayToDoItems (content, currentProject) {
         e.preventDefault();
     });
 
-    updateToDoItems (currentProject, toDoContainer, 0);
+    //updateToDoItems (currentProject, toDoContainer, 0);
 
 }
 
@@ -168,6 +163,7 @@ function updateToDoItems (currentProject, container, position) {
 
     //Create Task Item
     const toDoItem = document.createElement("div");
+    toDoItem.setAttribute("class", "taskDiv");
     toDoItem.setAttribute("id", "taskID_" + position);
     toDoItem.style.padding = "5px";
     toDoItem.style.margin = "5px";
@@ -195,18 +191,30 @@ function updateToDoItems (currentProject, container, position) {
     const status = document.createElement("input");
     status.style.width = "auto";
     status.setAttribute("type", "checkbox");
+    status.setAttribute("class", "statusInput");
+    status.setAttribute("id", "statusID_" + position);
+    currentProject.toDoItems[position].checkmark = status;
+
     status.style.transform = "scale(1.5)";
     status.style.margin = "5px";
     titleDiv.appendChild(status);
 
     //Checkbox listener for status
     status.addEventListener("change", function(e) {
+        const currentCheck = e.target.id;
+        const currentCheckID = currentCheck.split('_')[1];
+
         if (e.target.checked) {
             toDoItem.style.backgroundColor = "#a9f7c7";
+            currentProject.toDoItems[currentCheckID].status = true;
         } else {
             toDoItem.style.backgroundColor = "";
+            currentProject.toDoItems[currentCheckID].status = false;
         }
+
     });
+
+   
 
     //create edit/delete element
     const editDelDiv = document.createElement("div");
@@ -214,6 +222,11 @@ function updateToDoItems (currentProject, container, position) {
     editDelDiv.style.justifyContent = "space-around";
     
     const editBtn = document.createElement("button");
+    editBtn.setAttribute("class", "editBtn");
+    editBtn.setAttribute("id", "editID_" + position);
+
+    currentProject.toDoItems[position].editBtn = editBtn;
+    
     editBtn.textContent = "Edit";
     editDelDiv.appendChild(editBtn);
 
@@ -221,24 +234,23 @@ function updateToDoItems (currentProject, container, position) {
     const dialog = document.querySelector("#addItemDialog");
 
 
-
-
     editBtn.addEventListener('click', (e) => {
         // dialogForm.reset();
         // When the user clicks the confirm button, close the dialog
-
-        document.querySelector("#taskTitle").value = currentProject.toDoItems[position].title;
-        document.querySelector("#taskDescription").value = currentProject.toDoItems[position].description;
-        document.querySelector("#priorityLevel").value = currentProject.toDoItems[position].priority;
+        const currentButton = e.target.id;
+        const currentButtonID = currentButton.split('_')[1];
+        document.querySelector("#taskTitle").value = currentProject.toDoItems[currentButtonID].title;
+        document.querySelector("#taskDescription").value = currentProject.toDoItems[currentButtonID].description;
+        document.querySelector("#priorityLevel").value = currentProject.toDoItems[currentButtonID].priority;
         
         confirmBtn.onclick = function(e) {
             //add an item function
             console.log("CONFIRM");
-            if(editToDo(currentProject, container, position)){
+            if(editToDo(currentProject, container, currentButtonID)){
                 dialog.close();
-                toDoTitle.textContent = currentProject.toDoItems[position].title;
-                toDoDescription.textContent = currentProject.toDoItems[position].description;
-                priorityLevel.textContent = currentProject.toDoItems[position].priority;
+                toDoTitle.textContent = currentProject.toDoItems[currentButtonID].title;
+                toDoDescription.textContent = currentProject.toDoItems[currentButtonID].description;
+                priorityLevel.textContent = currentProject.toDoItems[currentButtonID].priority;
             }
                 
             e.preventDefault();
@@ -258,19 +270,41 @@ function updateToDoItems (currentProject, container, position) {
 
 
     const delBtn = document.createElement("button");
+    delBtn.setAttribute("class", "delBtn");
+    delBtn.setAttribute("id", "delID_" + position);
+
+    currentProject.toDoItems[position].delBtn = delBtn;
+
     delBtn.textContent = "Delete";
     editDelDiv.appendChild(delBtn);
 
-    //Checkbox listener for status
-    // statusDiv.addEventListener("change", function(e) {
-    //     if (e.target.checked) {
-    //         toDoItem.style.backgroundColor = "#a9f7c7";
-    //     } else {
-    //         toDoItem.style.backgroundColor = "";
-    //     }
-    // });
+    delBtn.addEventListener('click', (e) => {
+        if (confirm("Are you sure you want to remove this task?") == true) {
+            const currentButton = e.target.id;
+            const currentButtonID = currentButton.split('_')[1];
 
+            let taskToRemoveID = "#taskID_" + currentButtonID;
+            let taskToRemove = document.querySelector(taskToRemoveID);
+            container.removeChild(taskToRemove);
+            currentProject.toDoItems.splice(currentButtonID,1);
+            
+            let taskDivs = document.querySelectorAll(".taskDiv");
+            let editBtns = document.querySelectorAll(".editBtn");
+            let delBtns = document.querySelectorAll(".delBtn");
+            let statusCheckmarks = document.querySelectorAll(".statusInput");
 
+            for (let k = 0; k < currentProject.toDoItems.length; k++)
+            {
+                taskDivs[k].setAttribute("id", "taskID_" + k);
+                editBtns[k].setAttribute("id", "editID_" + k);
+                delBtns[k].setAttribute("id", "delID_" + k);
+                statusCheckmarks[k].setAttribute("id", "statusID_" + k);
+            }
+
+            console.log(currentProject.toDoItems);
+        }
+        e.preventDefault();
+    });
 
 
     //Append all items
